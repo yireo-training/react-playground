ff5926b42e647a5c89ca5161c7caa0e2bba59c8b
jisse Reitsma <jisse@yireo.com>
2020-08-17 13:31:59 +0200

    Add clear interval

--- src/App/Clock/ClockFunctional.js
+++ src/App/Clock/ClockFunctional.js
@@ -6,7 +6,10 @@ const Clock = () => {
   const [time, setTime] = useState(getTime());
 
   useEffect(() => {
-    setInterval(() => setTime(getTime()), 1000);
+    const timer = setInterval(() => setTime(getTime()), 1000);
+    return function() {
+      clearInterval(timer);
+    }
   }, []);
 
   return <h2>It is {time}.</h2>;
